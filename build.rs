9dffd6d4e04f070c9507630071c2cda93a39fa3e
Vahe Danielyan <vahe.danielyan@aerodynamics.am>
2024-01-06 17:56:31 +0400

    Place man file to OUT_DIR

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
 use clap_mangen::Man;
+use std::env;
 
 include!("src/args.rs"); // Adjust the path to where your Cli struct is defined
 
 fn main() {
     let app = Args::command();
-    let mut file = File::create("dirdiff.1").expect("Could not create man page file");
+    let out_dir = std::path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut file = File::create(out_dir.join("dirdiff.1")).expect("Could not create man page file");
     Man::new(app).render(&mut file).expect("Could not render man page");
 }
 
